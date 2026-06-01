49614853a3dcfebfd6e513b80a801f8ad54df654
Mange Joe <madzira3@gmail.com>
2026-06-01 14:30:36 +0200

    added user service. also added needed methods for communicating with api

---
+++ OnlineLibrary/Services/IUserService.cs
@@ -0,0 +1,19 @@
+﻿
+using OnlineLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.Services
+{
+    public interface IUserService
+    {
+        public Task<bool> CreateUser(OverallUser Ouser);
+        public Task UpdateUser(int id,OverallUser Ouser);
+        public Task<bool> DeleteUser(int id);
+        public Task GetUserById(int id);
+        public Task GetUsers();
+    }
+}
---
+++ OnlineLibrary/Services/UserService.cs
@@ -0,0 +1,74 @@
+﻿using OnlineLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly HttpClient _httpClient;
+        public UserService()
+        {
+            _httpClient = new()
+            {
+                BaseAddress= new Uri("https://localhost:7196")
+            };
+        }
+        async Task<bool> IUserService.CreateUser(OverallUser Ouser)
+        {
+            try
+            {
+                var response = await _httpClient.PostAsJsonAsync("/api/User", Ouser);
+                if (response.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlert("error",ex.Message,"Ok");
+                return false;
+            }
+           
+            
+        }
+
+        async Task<bool> IUserService.DeleteUser(int id)
+        {
+            try
+            {
+                var response = await _httpClient.DeleteAsync($"/api/User/{id}");
+                if (response.IsSuccessStatusCode)
+                    return true;
+                return false;
+            }
+            catch (Exception ex) 
+            {
+                await Shell.Current.DisplayAlert("error", ex.Message, "Ok");
+                return false;
+            }
+           
+
+        }
+
+        Task IUserService.GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IUserService.GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IUserService.UpdateUser(int id, OverallUser Ouser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
