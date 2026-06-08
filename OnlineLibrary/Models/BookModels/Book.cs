064c96ad509ea50684d6a41ceec300607a1e3bda
Mange Joe <madzira3@gmail.com>
2026-06-08 16:42:16 +0200

    added book models

---
+++ OnlineLibrary/Models/BookModels/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.Models.BookModels
+{
+    public class Book
+    {
+
+        public int Book_Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ISBN { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public string Edition { get; set; } = string.Empty;
+        public string Publisher { get; set; } = string.Empty;
+        public DateTime Publication_Date { get; set; } = DateTime.Now;
+        public string Availability_Status { get; set; } = string.Empty;
+    }
+}
---
+++ OnlineLibrary/Models/BookModels/BookCopy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.Models.BookModels
+{
+    public class BookCopy
+    {
+        public int Copy_Id { get; set; }
+        public string Condition { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public int Book_Id { get; set; } //this is the foreign key 
+
+    }
+}
