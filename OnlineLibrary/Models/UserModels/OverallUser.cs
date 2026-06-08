6a8666b70efc26c341cd3e88bda28e5820092cde
Mange Joe <madzira3@gmail.com>
2026-06-08 16:43:07 +0200

    moved user models to userModel folder

--- OnlineLibrary/Models/UserModels/OverallUser.cs
+++ OnlineLibrary/Models/UserModels/OverallUser.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OnlineLibrary.Models
+namespace OnlineLibrary.Models.UserModels
 {
     public class OverallUser
     {
