0d8a9f315b727c31cab3a01deb49404ff4c2037d
Mange Joe <madzira3@gmail.com>
2026-06-16 17:11:44 +0200

    added functionality but not yet working

--- OnlineLibrary/ViewModels/BookViewModels/CatalogueViewModel.cs
+++ OnlineLibrary/ViewModels/BookViewModels/CatalogueViewModel.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using OnlineLibrary.Models.BookModels;
+using OnlineLibrary.Services;
+using OnlineLibrary.Views.BookPages;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace OnlineLibrary.ViewModels.BookViewModels
 {
-    internal class CatalogueViewModel
+    public class CatalogueViewModel: IQueryAttributable
     {
+        public BookService _bookService;
+        public ObservableCollection<BookDetailsViewModel> allBooks {  get; set; }
+        public ICommand AddNewBookCommand { get; set; }
+        public ICommand BookDetailsCommand { get; set; }
+
+        public CatalogueViewModel(BookService bookService)
+        {
+            _bookService = bookService;
+            allBooks=new ObservableCollection<BookDetailsViewModel>();
+            AddNewBookCommand = new AsyncRelayCommand(AddNewBook);
+            BookDetailsCommand = new AsyncRelayCommand<BookDetailsViewModel>(GoToDetails);
+
+        }
+
+        private async Task GoToDetails(BookDetailsViewModel? model)
+        {
+            await Shell.Current.GoToAsync($"{nameof(BookDetailsPage)}?bookId={model.Book_Id}");
+        }
+
+        private async Task AddNewBook()
+        {
+            await Shell.Current.GoToAsync(nameof(AddBookPage));
+        }
+
+        void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
